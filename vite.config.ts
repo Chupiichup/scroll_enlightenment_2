98f73b27bdd390ba6f6bc9bf2e6d9ec0d98b34e7
chupiichup <chuthao112@gmail.com>
2026-04-10 11:34:09 +0700

    Auto update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "/https-github.com-Chupiichup-scroll_enlightenment/",
+    base: "/scroll_enlightenment_2/",
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
